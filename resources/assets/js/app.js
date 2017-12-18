872d95fb0cc0525b7c9fb398c1939c0f62e47fc0
fedchishina.victoria@gmail.com <fedchishina.victoria@gmail.com>
2017-12-18 11:54:09 +0300

    fixed bugs in department function installed laravel-lang

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -21,72 +21,53 @@ const app = new Vue({
     el: '#app'
 });
 
+var $form = $('.form');
+var $errBlock = $form.find('.error-list');
+var $modalDel = $('#modal-container-delete-department');
 
-$( function() {
-    'use strict';
-    var $form = $('.form');
-    var $errBlock = $form.find('.error-list');
+$(document).on("click", ".btn-delete", function() {
+    var $this = $(this);
+    $errBlock.html('');
+    $modalDel.find( "input[name='id']" ).val( $this.data('id') );
+});
 
-    init();
+$(document).on("click", ".btn-edit", function() {
+    var $this = $(this);
+    $errBlock.html('');
+    var params = $this.data('params');
+    var $modalEdit = $('#modal-container-edit-department');
+    $modalEdit.find( "input[name='id']" ).val(params.id );
+    $modalEdit.find( "input[name='name']" ).val(params.name );
+});
 
-    function init() {
-        initDepartment();
-        initForms();
-    }
-
-    function initDepartment(){
-        var $btnDelInTable = $('.table .btn-delete');
-        $btnDelInTable.on('click', function () {
-            $errBlock.html('');
-            var $this = $(this);
-            var $modalDel = $('#modal-container-delete-department');
-            //filling the form with url parameters
-            $modalDel.find( "input[name='id']" ).val( $this.data('id') );
-        });
-
-        var $btnEditInTable = $('.table .btn-edit');
-        $btnEditInTable.on('click', function () {
-            $errBlock.html('');
-            var $this = $(this);
-            var params = $this.data('params');
-            var $modalEdit = $('#modal-container-edit-department');
-            $modalEdit.find( "input[name='id']" ).val(params.id );
-            $modalEdit.find( "input[name='name']" ).val(params.name );
-        });
-    }
-
-    function initForms() {
-        $form.on('submit', function (event) {
-            var $this = $(this);
-            event.preventDefault();
-            $errBlock.html('');
-            $.ajax({
-                url: $this.attr('action'),
-                method: $this.attr('method'),
-                data: $this.serialize(),
-                dataType: 'html'
-            }).done(function (data) {
-                //close modal form
-                $(".form .btn-close").trigger("click");
-                //update content of table
-                $('.table-content').html(data);
-                init();
-            }).fail(function (err) {
-                //validation errors
-                if (err.status == 422) {
-                    var errorList = '<ul>';
-                    var errors = JSON.parse(err.responseText);
-                    //getting list of errors
-                    for(var prop in errors) {
-                        if(!!errors[prop]) {
-                            errorList += '<li><strong>' + errors[prop] + '</strong></li>';
-                        }
-                    }
-                    errorList += '</ul>';
-                    //viewing list of errors on edit short url form
-                    $errBlock.html(errorList);
+$form.on('submit', function (event) {
+    var $this = $(this);
+    event.preventDefault();
+    $errBlock.html('');
+    $.ajax({
+        url: $this.attr('action'),
+        method: $this.attr('method'),
+        data: $this.serialize(),
+        dataType: 'html'
+    }).done(function (data) {
+        //close modal form
+        $(".form .btn-close").trigger("click");
+        //update content of table
+        $('.table-content').html(data);
+    }).fail(function (err) {
+        //validation errors
+        if (err.status == 422) {
+            var errorList = '<ul>';
+            var errors = JSON.parse(err.responseText)['errors'];
+            //getting list of errors
+            for(var prop in errors) {
+                if(!!errors[prop]) {
+                    errorList += '<li><strong>' + errors[prop] + '</strong></li>';
                 }
-            })
-        });
-    }
+            }
+            errorList += '</ul>';
+            //viewing list of errors on edit short url form
+            $errBlock.html(errorList);
+        }
+    })
 });
